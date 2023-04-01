f5411e25bde7c78d9a9550cfe2639dacf56e21c4
gaberay2002 <gaberay2002@yahoo.com>
2023-04-01 15:13:20 -0500

    final code to be merged into main

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,29 +1,69 @@
-#include <Servo.h>
+#include <Servo.h> // import servo library
+#include <NewPing.h> // import sonar sensor library
+#include <LiquidCrystal.h> // import lcd library
+#define ECHO_PIN 9 // set echo pin for sonar
+#define TRIGGER_PIN 8 // set trigger pin for sonar
+#define MAX_DISTANCE 400 //set maximum distance of sonar in cm
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-String myCmd;
-int pos = 0;    // variable to store the servo position
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); //Create new sonar object
+
+Servo myservo;  // create servo objects to control a servo
+Servo myservo2;
+Servo myservo3;
+
+String myCmd; //create string to take in python values
+
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2); //LCD oject
 
 void setup() {
-  Serial.begin(115200);
-  myservo.attach(12);// attaches the servo on pin 9 to the servo object
+  Serial.begin(115200); // set baud
+  lcd.begin(16,2); //set dimensions of lcd
+  myservo.attach(13); //attach servo on pins
+  myservo2.attach(12);
+  myservo3.attach(10);
 }
 
 void loop() {
-  while(Serial.available()==0){
+  while(Serial.available()==0){ //loop when not taking in input
+    lcd.clear(); // clear lcd screen
+    lcd.setCursor(0, 0); // set lcd cursor back to start
+    lcd.print("Have a good Day!"); // print text to lcd
+    myservo.write(10); //move servos to angle position
+    myservo2.write(10);
+    myservo3.write(10);
+    delay(1000); // 1 second delay
+    Serial.print(sonar.ping_cm()); //output echolocation data as distance from object to sensor in cm
+    Serial.print("\r\n"); // formating for delminiation in python file and nice out for distance
+  }
+  
+  myCmd=Serial.readStringUntil('\r'); //set the myCmd string to our python data
+  if(myCmd=="0"){ //python input of 0 (Compost)
+        lcd.clear(); // clear lcd
+        lcd.setCursor(0, 0); // set lcd cursor to start
+        lcd.print("Compost"); // print text to lcd
+        myservo.write(360);   //move servo to angle position
+        delay(3000);  // 3s delay
 
   }
-  myCmd=Serial.readStringUntil('\r');
-  if(myCmd=="1"){
-    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(1);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(1);                       // waits 15ms for the servo to reach the position
-    }
+  if(myCmd=="1"){ //python input of 1 (Trash)
+        lcd.clear(); // clear lcd
+        lcd.setCursor(0, 0); // set lcd cursor to start
+        lcd.print("Trash"); // print text to lcd
+        myservo2.write(360); // move servo to angle position
+        delay(3000); // 3s delay
   }
+  if(myCmd=="2"){ // python input of 2 (Recycle)
+        lcd.clear(); // clear lcd
+        lcd.setCursor(0, 0); // set lcd position to start
+        lcd.print("Recycle"); // print text to lcd
+        myservo3.write(360);     // move servo to angle position
+        delay(3000);  // 3s delay
+  }
+   if(myCmd=="3"){ // python input of 3 for people (default mode incase people get to close)
+        myservo.write(10);    // move servo to angle position
+        myservo2.write(10);       
+        myservo3.write(10);        
+        delay(1);  
+   }
 }
--- PythonToArduinoTest.py
+++ PythonToArduinoTest.py
@@ -1,9 +1,109 @@
 #Currently only allows basic input from python file to be read by arduino
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import cv2  # import opencv-python
+import numpy as np # import numpy for arrays
+import time #import time for delays
+import serial #import serial for arduino communication
 
-import serial
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
+
+# CAMERA can be 0 or 1 based on default camera of your computer
+camera = cv2.VideoCapture(1)
+
+#set up arduino calls on com3 port with correct baud
 arduinoData=serial.Serial('com3', 115200)
 
+def returnvalues(value): #function to return integer value that will be read into arduino
+    if value == 0:
+        return 0 #compost
+    if value == 1:
+        return 1 #trash
+    if value == 2:
+        return 2 #recycle
+    if value == 3:
+        return 3 #people
+    
 while True:
-    cmd=input()
-    cmd=cmd+'\r'
-    arduinoData.write(cmd.encode())
+    while(arduinoData.inWaiting()==0): #while statment to keep video feed going when not recieving arduino values
+        cmd="5" #give default state to arduino
+        cmd=cmd+'\r' #add delimiter char
+        arduinoData.write(cmd.encode()) #send to arduino
+        # Grab the webcamera's image.
+        ret, image = camera.read()
+
+        # Resize the raw image into (224-height,224-width) pixels
+        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+        # Show the image in a window
+        cv2.imshow("Webcam Image", image)
+
+        # # Make the image a numpy array and reshape it to the models input shape.
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+        # # Normalize the image array
+        image = (image / 127.5) - 1
+
+        # # Predicts the model
+        prediction = model.predict(image)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
+
+        # # Listen to the keyboard for presses.
+        keyboard_input = cv2.waitKey(1)
+
+    cmd="5" #set default state at start when recieving arduino values incase of mulitple inputs in a row
+    cmd=cmd+'\r' #add delimieter char
+    arduinoData.write(cmd.encode()) #send to arduion
+    # Grab the webcamera's image.
+    ret, image = camera.read()
+
+    # Resize the raw image into (224-height,224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Show the image in a window
+    cv2.imshow("Webcam Image", image)
+
+    # Make the image a numpy array and reshape it to the models input shape.
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predicts the model
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    print("Class:", class_name[2:], end="")
+    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+    # Listen to the keyboard for presses.
+    keyboard_input = cv2.waitKey(1)
+
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break
+
+    dataPacket = arduinoData.readline() #read in arduino values
+    dataPacket = str(dataPacket, 'utf-8') #set arduino values to utf-8 string
+    dataPacket = dataPacket.strip('\r\n') #delimit
+    x=float(dataPacket) #convert floating point number
+    print(dataPacket) #print sonor values for distance in debugging
+    if not x < 5 and x <= 25: #if equal or less than 25 cm away and greater than 5 cm away 
+        values=returnvalues(index) #get value relating to trash shoot to send to arduino
+        cmd=f"{values}" #set num value to string in or
+        cmd=cmd+'\r' #add delimiter value
+        arduinoData.write(cmd.encode()) #send to arduino
+        time.sleep(4) #pause python program for delay
+
+camera.release() #close camera access when esc key pressed
+cv2.destroyAllWindows() #destroys cv window
